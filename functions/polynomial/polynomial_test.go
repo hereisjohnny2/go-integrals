ac0b88c311ca2cdac724fc1ed12f5e2786a60e12
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2022-01-18 21:24:48 -0300

    create test for set and get params from polynominal

--- functions/polynomial/polynomial_test.go
+++ functions/polynomial/polynomial_test.go
@@ -42,10 +42,36 @@ func TestF(t *testing.T) {
 }
 
 func TestExpression(t *testing.T) {
-	expr := NewPolynomial(makeMockParams()).Expression()
+	poly := NewPolynomial(makeMockParams())
+
+	expr := poly.Expression()
 	expected := "f(x)=1+2x^1+3x^2+4x^3"
 
 	if expr != expected {
 		t.Errorf("Expected to be %v. Got %v.", expected, expr)
+	}
+}
+
+func TestGetParams(t *testing.T) {
+	params := NewPolynomial(makeMockParams()).GetParamters()
+	expected := makeMockParams()
+
+	if params["a"] != expected["a"] {
+		t.Errorf("Expected to be %v. Got %v.", params, expected)
+	}
+}
+
+func TestSetParams(t *testing.T) {
+	poly := NewPolynomial(makeMockParams())
+	expected := map[string]float64{
+		"a": 5,
+		"b": 5,
+	}
+
+	poly.SetParameters(expected)
+	params := poly.params
+
+	if params["a"] != expected["a"] && params["b"] != expected["b"] {
+		t.Errorf("Expected to be %v. Got %v.", params, expected)
 	}
 }
